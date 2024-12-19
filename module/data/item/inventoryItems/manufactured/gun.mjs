4f1383eec72f6293664d8f61d3d2a3e5e0f84522
CarnVanBeck <carnvanbeck@gmail.com>
2024-12-19 13:52:36 +0100

    added type, changed field type for redtext and prefix to DocumentIdField (hope that will work)

--- module/data/item/inventoryItems/manufactured/gun.mjs
+++ module/data/item/inventoryItems/manufactured/gun.mjs
@@ -7,26 +7,26 @@ export default class GunItemData extends ManufacturedItemData {
     static defineSchema() {
         const fields = foundry.data.fields;
         const schema = super.defineSchema();
-        schema.level = new fields.StringField({ label: game.i18n.localize('badass.item.gun.level') });
-        schema.damage = new fields.StringField({ label: game.i18n.localize('badass.item.gun.damage') });
-        schema.range = new fields.NumberField({ label: game.i18n.localize('badass.item.gun.range') });
-        schema.redText = new fields.ObjectField({
-            name: 'redText',
-            label: game.i18n.localize('badass.item.gun.redText'),
+        schema.level = new fields.StringField({ label: 'badass.item.gun.level' });
+        schema.damage = new fields.StringField({ label: 'badass.item.gun.damage' });
+        schema.range = new fields.NumberField({ label: 'badass.item.gun.range' });
+        schema.redText = new fields.DocumentIdField(foundry.documents.BaseItem, {
+            label: 'badass.item.gun.redText',
             required: false,
             nullable: true,
         });
-        schema.prefix = new fields.ObjectField({
-            name: 'prefix',
-            label: game.i18n.localize('badass.item.gun.prefix'),
+        schema.prefix = new fields.DocumentIdField(foundry.documents.BaseItem, {
+            label: 'badass.item.gun.prefix',
             required: false,
             nullable: true,
         });
         schema.accuracy = new fields.ArrayField(new fields.ObjectField());
-        schema.type = new fields.StringField({ label: game.i18n.localize('badass.item.gun.type') });
+        schema.type = new fields.StringField({ label: 'badass.item.gun.type' });
         return schema;
     }
 
     /** @inheritDoc */
-    prepareDerivedData() {}
+    prepareDerivedData() {
+        this.type = 'gun';
+    }
 }
