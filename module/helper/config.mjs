e513efe10d12bee00c2e73cee3c9001c968a9e5e
CarnVanBeck <carnvanbeck@gmail.com>
2025-02-09 20:51:34 +0100

    removed stuff that could be copyrighted, some formatting and added get method for vh levels

--- module/helper/config.mjs
+++ module/helper/config.mjs
@@ -1,8 +1,5 @@
 import Defense from '../data/custom/defense.mjs';
 import Element from '../data/custom/element.mjs';
-import GunType from '../data/custom/gunType.mjs';
-import GunLevel from '../data/custom/gunLevel.mjs';
-import GunAccuracy from '../data/custom/gunAccuracy.mjs';
 
 export const BADASS = {};
 BADASS.namespace = 'vtt-bunkersandbadasses';
@@ -11,25 +8,25 @@ BADASS.systemPath = 'systems/vtt-bunkersandbadasses';
 export function getDefaultDefenses() {
     let shield = new Defense();
     shield.key = 'sld';
-    shield.name = 'badass.defense.shield.label';
-    shield.description = 'badass.defense.shield.hint';
+    shield.name = 'Shield';
+    shield.description = 'Used by Shields and takes double damage from Shock damage.';
     shield.requiresItem = true;
     shield.order = 0;
     let armor = new Defense();
     armor.key = 'arm';
-    armor.name = 'badass.defense.armor.label';
-    armor.description = 'badass.defense.armor.hint';
+    armor.name = 'Armor';
+    armor.description = 'Used for actors with the Armored trait and takes double damage from Corrosive damage.';
     armor.order = 10;
     let hp = new Defense();
     hp.key = 'hp';
-    hp.name = 'badass.defense.hp.label';
-    hp.description = 'badass.defense.hp.hint';
+    hp.name = 'Health';
+    hp.description = 'Used for actors with the Flesh trait and takes double damage from Incendiary damage.';
     hp.isLife = true;
     hp.order = 20;
     let generic = new Defense();
     generic.key = 'gen';
-    generic.name = 'badass.defense.generic.label';
-    generic.description = 'badass.defense.generic.hint';
+    generic.name = 'Health';
+    generic.description = "Used if the actor doesn't have any weaknesses against any element.";
     generic.isLife = true;
     generic.order = 30;
     return [shield, armor, hp, generic];
@@ -38,99 +35,77 @@ export function getDefaultDefenses() {
 export function getDefaultElements() {
     let incendiary = new Element();
     incendiary.key = 'inc';
-    incendiary.name = 'badass.element.incendiary.label';
-    incendiary.description = 'badass.element.incendiary.hint';
+    incendiary.name = 'Incendiary';
+    incendiary.description = '';
     incendiary.strongAgainst = ['hp'];
     let corrosive = new Element();
     corrosive.key = 'cor';
-    corrosive.name = 'badass.element.corrosive.label';
-    corrosive.description = 'badass.element.corrosive.hint';
+    corrosive.name = 'Corrosive';
+    corrosive.description = '';
     corrosive.strongAgainst = ['arm'];
     let shock = new Element();
     shock.key = 'shk';
-    shock.name = 'badass.element.shock.label';
-    shock.description = 'badass.element.shock.hint';
+    shock.name = 'Shock';
+    shock.description = '';
     shock.strongAgainst = ['sld'];
     let explosive = new Element();
     explosive.key = 'xpl';
-    explosive.name = 'badass.element.explosive.label';
-    explosive.description = 'badass.element.explosive.hint';
+    explosive.name = 'Explosive';
+    explosive.description = '';
     let radiation = new Element();
     radiation.key = 'rad';
-    radiation.name = 'badass.element.radiation.label';
-    radiation.description = 'badass.element.radiation.hint';
+    radiation.name = 'Radiation';
+    radiation.description = '';
     radiation.ignores = ['sld', 'arm'];
     let cryo = new Element();
     cryo.key = 'cry';
-    cryo.name = 'badass.element.cryo.label';
-    cryo.description = 'badass.element.cryo.hint';
+    cryo.name = 'Cryo';
+    cryo.description = '';
 
     return [incendiary, corrosive, shock, explosive, radiation, cryo];
 }
 
-export function getDefaultGunTypes() {
-    let combatRifle = new GunType();
-    combatRifle.key = 'cmb';
-    combatRifle.name = 'badass.item.gun.types.combatRifle.label';
-    combatRifle.description = 'badass.item.gun.types.combatRifle.hint';
-    combatRifle.levels = combatRifleLevels();
-
-    return [combatRifle];
-}
-
 export function getDefaultRarities() {
-    let common = { name: 'common', key: 'common', icon: "systems/vtt-bunkersandbadasses/assets/standalone/rarity/common.svg"};
-    let uncommon = { name: 'uncommon', key: 'uncommon', icon: "systems/vtt-bunkersandbadasses/assets/standalone/rarity/uncommon.svg" };
-    let rare = { name: 'rare', key: 'rare', icon: "systems/vtt-bunkersandbadasses/assets/standalone/rarity/rare.svg" };
-    let epic = { name: 'epic', key: 'epic', icon: "systems/vtt-bunkersandbadasses/assets/standalone/rarity/epic.svg" };
-    let legendary = { name: 'legendary', key: 'legendary', icon: "systems/vtt-bunkersandbadasses/assets/standalone/rarity/legendary.svg" };
-    return [common, uncommon, rare, epic, legendary];
+    return [
+        { name: 'common', key: 'common', icon: `${BADASS.systemPath}/assets/standalone/rarity/common.svg` },
+        { name: 'uncommon', key: 'uncommon', icon: `${BADASS.systemPath}/assets/standalone/rarity/uncommon.svg` },
+        { name: 'rare', key: 'rare', icon: `${BADASS.systemPath}/assets/standalone/rarity/rare.svg` },
+        { name: 'epic', key: 'epic', icon: `${BADASS.systemPath}/assets/standalone/rarity/epic.svg` },
+        { name: 'legendary', key: 'legendary', icon: `${BADASS.systemPath}/assets/standalone/rarity/legendary.svg` },
+    ];
 }
 
-function combatRifleLevels() {
-    let level1 = new GunLevel();
-    level1.start = 1;
-    level1.end = 6;
-    level1.damage = '1d6';
-    level1.range = 6;
-    level1.accuracy = [gunAccuracy(2, 7, 1, 0), gunAccuracy(8, 15, 3, 0), gunAccuracy(16, null, 3, 1)];
-
-    let level2 = new GunLevel();
-    level2.start = 7;
-    level2.end = 12;
-    level2.damage = '1d8';
-    level2.range = 6;
-    level2.accuracy = [gunAccuracy(2, 7, 2, 0), gunAccuracy(8, 15, 3, 0), gunAccuracy(16, null, 2, 1)];
-
-    let level3 = new GunLevel();
-    level3.start = 13;
-    level3.end = 18;
-    level3.damage = '1d8';
-    level3.range = 6;
-    level3.accuracy = [gunAccuracy(2, 7, 1, 1), gunAccuracy(8, 15, 2, 1), gunAccuracy(16, null, 2, 2)];
-
-    let level4 = new GunLevel();
-    level4.start = 19;
-    level4.end = 24;
-    level4.damage = '2d6';
-    level4.range = 6;
-    level4.accuracy = [gunAccuracy(2, 7, 1, 0), gunAccuracy(8, 15, 2, 1), gunAccuracy(16, null, 3, 1)];
-
-    let level5 = new GunLevel();
-    level5.start = 25;
-    level5.end = 30;
-    level5.damage = '1d10';
-    level5.range = 6;
-    level5.accuracy = [gunAccuracy(2, 7, 1, 2), gunAccuracy(8, 15, 2, 1), gunAccuracy(16, null, 2, 3)];
-
-    return [level1, level2, level3, level4, level5];
-}
-
-function gunAccuracy(low, high, hits, crits) {
-    let retVal = new GunAccuracy();
-    retVal.low = low;
-    retVal.high = high;
-    retVal.hits = hits;
-    retVal.crits = crits;
-    return retVal;
+export function getDefaultVaultHunterLevels() {
+    return [
+        { level: 1, xpSegment: 100 },
+        { level: 2, xpSegment: 100 },
+        { level: 3, xpSegment: 100 },
+        { level: 4, xpSegment: 150 },
+        { level: 5, xpSegment: 150 },
+        { level: 6, xpSegment: 200 },
+        { level: 7, xpSegment: 200 },
+        { level: 8, xpSegment: 200 },
+        { level: 9, xpSegment: 250 },
+        { level: 10, xpSegment: 250 },
+        { level: 11, xpSegment: 300 },
+        { level: 12, xpSegment: 300 },
+        { level: 13, xpSegment: 300 },
+        { level: 14, xpSegment: 350 },
+        { level: 15, xpSegment: 350 },
+        { level: 16, xpSegment: 400 },
+        { level: 17, xpSegment: 400 },
+        { level: 18, xpSegment: 400 },
+        { level: 19, xpSegment: 450 },
+        { level: 20, xpSegment: 450 },
+        { level: 21, xpSegment: 500 },
+        { level: 22, xpSegment: 500 },
+        { level: 23, xpSegment: 500 },
+        { level: 24, xpSegment: 550 },
+        { level: 25, xpSegment: 550 },
+        { level: 26, xpSegment: 600 },
+        { level: 27, xpSegment: 600 },
+        { level: 28, xpSegment: 600 },
+        { level: 29, xpSegment: 650 },
+        { level: 30, xpSegment: 650 },
+    ];
 }
